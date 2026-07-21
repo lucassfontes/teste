f17593e3b9ee3e48539ddc851cc994c5fc4e1a1a
lucas <lucassfontessantos@gmail.com>
2026-07-21 19:21:32 +0200

    Versão-3.4.10

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* VERSAO DO SISTEMA */
 const versao = document.getElementById("versao_sytem")
 
-versao.innerHTML = 'Versão-3.4.9'
+versao.innerHTML = 'Versão-3.4.10'
 /**
  * ARQUIVO PRINCIPAL DO VALLE
  * ------------------------------------------------
